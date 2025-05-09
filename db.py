adb03baba89b3fb19f0c33a3de9b8da6718e7ad0
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-09 22:46:01 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
-from sqlmodel import SQLModel, create_engine
-from models import User
+# db.py
 import os
+from sqlmodel import SQLModel, create_engine
+from models import User, ScanJob, ScanResult
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./confideai.db")
 engine = create_engine(DATABASE_URL, echo=True)
