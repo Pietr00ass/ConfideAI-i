544decebb6fdafe05d9839d66330b6636f11fe09
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-09 16:07:48 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 from sqlmodel import SQLModel, create_engine
-from models import User
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./confideai.db")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
+    # importujemy wszystkie modele, żeby je zarejestrować
     from models import User, ScanJob, ScanResult
     SQLModel.metadata.create_all(engine)
-
