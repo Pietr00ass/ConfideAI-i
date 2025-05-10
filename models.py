185f00d370db786181dd1cefd6db23803635d674
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-10 12:12:30 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from datetime import datetime
 from sqlmodel import SQLModel, Field, Relationship  # Poprawny import
+from sqlalchemy import Column, JSON, ForeignKey
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
