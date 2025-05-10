1c5741cd58db31886a3e3ce35a8b917f06cdfc5e
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-10 14:57:32 +0200

    Update models.py

--- models.py
+++ models.py
@@ -10,4 +10,13 @@ class User(SQLModel, table=True):
     is_active: bool = Field(default=False, index=True) 
     name: Optional[str] = None
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    
+
+class AnalysisResult(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    filename: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    emails: List[str] = Field(default_factory=list)        # np. JSON
+    pesel_numbers: List[str] = Field(default_factory=list) # JSON
+    credit_cards: List[str] = Field(default_factory=list)  # JSON
+    ml_predictions: Optional[str] = None
