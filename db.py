a4510af4f268ecf160f4fc9161b0cbe8aa08efeb
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-10 15:43:07 +0200

    Update db.py

--- db.py
+++ db.py
@@ -7,11 +7,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
-    # ręczne usuwanie starych tabel wraz z zależnościami
-    with engine.begin() as conn:
-        # kolejność nie ma znaczenia dzięki CASCADE
-        conn.execute(text('DROP TABLE IF EXISTS scanresult CASCADE;'))
-        conn.execute(text('DROP TABLE IF EXISTS scanjob CASCADE;'))
-        conn.execute(text('DROP TABLE IF EXISTS "user" CASCADE;'))
-    # teraz twórz tylko te tabele, które są w kodzie (User)
     SQLModel.metadata.create_all(bind=engine)
