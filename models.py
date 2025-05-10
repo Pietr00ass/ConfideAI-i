680590296d0a5f27eda7dea0c3b10137c1abcf3f
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-10 14:45:09 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from typing import Optional, List
 from datetime import datetime
-from sqlmodel import SQLModel, Field, Relationship  # Poprawny import
+from sqlmodel import SQLModel, Field, Relationship 
 from sqlalchemy import Column, JSON, ForeignKey
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True)
     password: str  # Pole dla hasła
+    is_active: bool = Field(default=False, index=True) 
     name: Optional[str] = None
     created_at: datetime = Field(default_factory=datetime.utcnow)
     
