f4b26f8d769139fb9872a90262445e66a2835ffd
Pietr00ass <76974388+Pietr00ass@users.noreply.github.com>
2025-05-11 19:29:31 +0200

    Update db.py

--- db.py
+++ db.py
@@ -7,8 +7,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
-    with engine.begin() as conn:
-        conn.execute(text('DROP TABLE IF EXISTS "user" CASCADE;'))
-        conn.execute(text('DROP TABLE IF EXISTS scanjob CASCADE;'))
-        conn.execute(text('DROP TABLE IF EXISTS scanresult CASCADE;'))
     SQLModel.metadata.create_all(bind=engine)
